d169e026fdbf148578ad352fd6cb81023a11db4e
一位史蒂夫 <pysgmy@outlook.com>
2021-06-12 01:04:55 +0800

    Create Label.js
    https://mzh.moegirl.org.cn/User:%E4%B8%80%E4%BD%8D%E5%8F%B2%E8%92%82%E5%A4%AB/JS/Label.js

---
+++ Label.js
@@ -0,0 +1,10 @@
+/*
+Label.js
+by User:AnnAngela
+Modified by User:一位史蒂夫
+ 
+Original script: https://zh.moegirl.org.cn/User:AnnAngela/js/PersonalLabel.js
+*/
+$(function() {
+    $('#pt-watchlist').before('<li id="pt-todo"><a href="' + mw.config.get("wgServer") + mw.config.get("wgScriptPath") + '/User:' + mw.config.get("wgUserName") + '/ToDoList">待办任务</a></li>');
+});
