e32013bacbbb9be6e1525707b532f1be710670fd
一位史蒂夫 <pysgmy@outlook.com>
2021-12-04 04:36:43 +0800

    Create Gadget-CheckModeratorActivities.js

---
+++ Gadget-CheckModeratorActivities.js
@@ -0,0 +1,6 @@
+// <pre>
+"use strict";
+if (mw.config.get("wgPageName") === "Special:维护组最后活跃列表") {
+    mw.loader.load('https://cdn.jsdelivr.net/gh/pysgmy/moegirlpedia@main/CheckModeratorActivities.min.js');
+}
+// </pre>
