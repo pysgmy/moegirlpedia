7b1c053b7012798989f2cac7e71aeddc6c3d0f11
一位史蒂夫 <pysgmy@outlook.com>
2022-01-10 15:18:40 +0800

    Update Label.js

--- Label.js
+++ Label.js
@@ -7,5 +7,5 @@ Original script: https://zh.moegirl.org.cn/User:AnnAngela/js/PersonalLabel.js
 */
 $(function() {
     $('#pt-watchlist').before('<li id="pt-todo"><a href="' + mw.config.get("wgServer") + mw.config.get("wgScriptPath") + '/User:' + mw.config.get("wgUserName") + '/ToDoList">待办任务</a></li>');
-    $('#pt-backlog').after('<li id="pt-activity"><a href="' + mw.config.get("wgServer") + mw.config.get("wgScriptPath") + '/Special:维护组最后活跃列表">内卷列表</a></li>');
+    $('#pt-personlog').after('<li id="pt-activity"><a href="' + mw.config.get("wgServer") + mw.config.get("wgScriptPath") + '/Special:维护组最后活跃列表">内卷列表</a></li>');
 });
